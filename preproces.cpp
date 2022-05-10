f3c1b42748b846c67b915c42a899832ea3fd978d
Marko Marfat <mm51762@fer.hr>
2022-05-10 22:34:49 +0200

    added construction of hash table

--- preproces.cpp
+++ preproces.cpp
@@ -33,6 +33,13 @@ string r_seq_L1 = "";  // all sequences concatenated with lowercase letters conv
 string r_seq_L3 = "";  // all sequences concatenated with other letters removed from t_seq_L2
 string r_final=""; // final sequence
 
+const int k = 20; // This is said to be the best value for k in the article
+const int max_hash_size = 1 << 20; // Maximum length of the hash table
+const int max_char_num = 1 << 28; // Maximum length of a chromosome
+
+int *point = new int[max_hash_size]; // An array of entries 
+int *loc = new int[max_char_num]; // An array of header pointers
+
 void target_preprocess(string file_name){
     //reading target FASTA file
     string line;
@@ -204,6 +211,40 @@ void refrence_preprocess(string file_name){
     }
 }
 
+
+/* This function constructs a hash table from the tuple values
+of a reference sequence.  */
+void initHT(){
+	// Initialize entries
+    for (int i = 0; i < max_hash_size; i++){
+        point[i] = -1;
+    }
+
+	uint64_t value = 0;
+	
+	// Only k values is needed, skip rest
+	for (int i = 0; i < k - 1; i++){
+		value = value << 2; // Not 100% sure about this one, I think it shifts the value so new char can be added (not really explained in the article)
+		// cout << "Value (1): " << value << endl; debugging
+		value = value + r_final[i]; // Add char to tuple
+		// cout << "Value (2): " << value << endl; debugging
+	}
+	
+	uint64_t remain = ((uint64_t) 1 << (2 * k)) - 1; // Used as a mask for which bits will remain (only those below 2 * k)
+	for (int i = k - 1; i < r_final.length(); i++){
+		value = value << 2;
+		// cout << "Value (1): " << value << endl; debugging
+		value = value + r_final[i];
+		// cout << "Value (2): " << value << endl; debugging
+		value = value & remain; // Only bits before 2 * k remain
+		int hash = value % max_hash_size; // By formula in the article [V mod s(size of hash table)]
+		loc[i - k + 1] =  point[hash]; // Tuple index has to be adjusted by k - 1
+		point[hash] = i - k + 1; 
+	}	
+
+}
+
+
 int main()
 {
     //target file preprocessing
@@ -213,6 +254,9 @@ int main()
     //reference file preprocessing
     refrence_preprocess("testref.txt");
     cout << "Encoded reference sequence: " << r_final << endl;
+	
+	initHT();
+    
     
     return 0;
 }
